045ce895ad3cc971773f884827256037908458f1
aljertal <aljertal@gmail.com>
2020-05-27 12:24:00 -0400

    reboot

--- greeting.js
+++ greeting.js
@@ -1,9 +1,25 @@
-// document.getElementById("para").innerHTML = sayHello(user);
-// class called 
+var createOptions = function (employeeOptions) {
+    var newOptions = {
+        FirstName: 'fistName5', Lastname: 'lastName5',
+        PhoneNumber: 'none ', Location: 'none', State: 'none',
+        ZipCode: 0, Occupation: 'none', HourlyWages: 33
+    };
+    if (employeeOptions) {
+        newOptions.PhoneNumber = employeeOptions.PhoneNumber;
+        newOptions.Location = employeeOptions.Location;
+        newOptions.State = employeeOptions.State;
+        newOptions.ZipCode = employeeOptions.ZipCode;
+        newOptions.Occupation = employeeOptions.Occupation;
+        newOptions.HourlyWages = employeeOptions.HourlyWages;
+    }
+    return newOptions;
+};
+// employee class
 var Employee = /** @class */ (function () {
-    // Constructor 
-    function Employee(firstName, lastName, phoneNumber, location, state, zipCode, occupation) {
-        this.hourlyWages = 50;
+    // constructor to initialize the props and 
+    function Employee(firstName, lastName, phoneNumber, location, state, zipCode, occupation, hourlyWages) {
+        // add hourlyWage property to person/Employee class
+        this.HourlyWages = 33;
         this.FirstName = firstName;
         this.Lastname = lastName;
         this.PhoneNumber = phoneNumber;
@@ -11,8 +27,9 @@ var Employee = /** @class */ (function () {
         this.State = state;
         this.ZipCode = zipCode;
         this.Occupation = occupation;
+        this.HourlyWages = hourlyWages;
     }
-    // methods to return combinations lik Full name, Name and Phone Number, Location, etc.
+    // methods to return combinations like Full name, Name and Phone Number, Location, etc.
     Employee.prototype.fullName = function () {
         return "Hi, this is " + this.FirstName + " " + this.Lastname;
     };
@@ -26,34 +43,40 @@ var Employee = /** @class */ (function () {
     };
     // All info
     Employee.prototype.fullInfo = function () {
-        return "So now, this is " + this.FirstName + " " + this.Lastname + " your can reach " + this.FirstName + this.PhoneNumber + " " + this.FirstName + " works as a " + this.Occupation + " in " + this.Location + ", " + this.State + " " + this.ZipCode;
+        return "So now, this is " + this.FirstName + " " + this.Lastname + " your can reach " + this.FirstName + this.PhoneNumber + " " + this.FirstName +
+            " works as a " + this.Occupation + " in " + this.Location + ", " + this.State + " " + this.ZipCode;
     };
-    // Add hourlyWage property to person 
-    // function that calculates a weekly wage and takes the number of hours as an optional parameter. // If not presented, use a typical 40hrs to calculate wage. Display this in a area that states name of person and weekly wage.
+    // weekly wage with num hours optional param. 
     Employee.prototype.totalWages = function (hours) {
-        if (hours === void 0) { hours = 40; }
-        return ("Name: " + this.FirstName + " " + this.Lastname + "\n Weekly Wage: $" + this.hourlyWages * hours);
+        return ("Name: " + this.FirstName + " " + this.Lastname + "\n Weekly Wage: $" + this.HourlyWages * hours);
     };
+    ;
     return Employee;
 }());
-// 5. Create several people and, output their information to <div> sections in html.
+// Create several people and, output their information to <div> sections in html.
 var employee1 = new Employee("Jimmy ", " Hendrix ", "704-771-7387", "Atlanta", "Georgia", 17501, "Developer");
 document.getElementById("e1").innerHTML = employee1.fullInfo();
-var employee2 = new Employee("Jonathan ", " Long ", "704-724-3817", "Charlotte", "North Carolina", 28212, "Analyst");
+var employee2 = new Employee("Jonathan ", " Long ", "704-724-3817", "Charlotte", "North Carolina", 28212, "Analyst", 12);
 document.getElementById("e2").innerHTML = employee2.fullInfo();
-var employee3 = new Employee("Noah ", "Jackson ", "516-523-7843", "Brooklyn", "New York", 11710, "Designer");
-document.getElementById("e3").innerHTML = employee3.fullInfo();
-var wages = new Employee("Jonathan ", " Long ", "704-724-3817", "Charlotte", "North Carolina", 28212, "Analyst");
-document.getElementById("e6").innerHTML = wages.totalWages();
-   
-// 4. Compile the file again with tsc to ensure the syntax is correct.
-// 5. Create several people and, mimicing the method above, assign the output of some of their information to various <div> sections in your page.
-// 6. Add a hourlyWage property to the person (and if you want, rename the class to Employee ). 
-// 7. Add a function that calculates a weekly wage and takes the number of hours as an optional parameter. If not presented, use a typical 40hrs to calculate wage. Display this in a area that states name of person and weekly wage.
-// NOTE: after each edit/save to the .ts file, you will have to compile and refresh the web page in the browser.
-// 8.Create a property for a person's certifications/degrees that's an array ex: MBA, CSM, MCSD, etc. 
-// 9.Create a function called addCerts() that takes an unknown number of
-// arguments and adds them to the array.
-// 10.Create an interface called EmployeeOptions (PersonOptions if you didn't rename to Employee) where the name values are required but the other parameters are optional. 
-// 11. Create a method in the class that takes an option parameter and returns an Employee (Person) based on the options given. Implement this will a few different options to observe how it works. For additional reference: https://www.typescriptlang.org/docs/handbook/interfaces.html#optional-properties
-// 12. Examine the compiled JavaScript file to see how the tsc changes TypeScript into JavaScript.
+var employee3 = new Employee("Noah ", "Jackson ");
+document.getElementById("e3").innerHTML = employee3.totalWages(12);
+// let employee13 = new Employee
+//  wages here
+// let wages = new Employee("Jonathan ", " Long ", "704-724-3817", "Charlotte", "North Carolina", 28212, "Analyst");
+// let employee = new Employee("Noah ", "Jackson ");
+// document.getElementById("e4").innerHTML = employee3.fullInfo();
+// wages = document.getElementById("e6");
+// If not presented, use a typical 40hrs to calculate wage. Display this in a area that states name of person and weekly wage.
+// const employee7 = createOptions({
+//     FirstName: 'fistName7', Lastname: 'lastName7', PhoneNumber: 'num7 ', Location: 'Location7', State: 'State7',
+//     ZipCode: 11710, Occupation: 'Occupation7'
+// });
+// let employee8 = new Employee(FirstName: 'firstName8', Lastname: 'lastName8' );
+// document.getElementById("e6").textContent = employee8.Occupation;
+// console.log(employee7);
+// console.log(employee8)
+function sayHello(person) {
+    return "Hello, " + person;
+}
+var user = "Super Student";
+document.getElementById("para").innerHTML = sayHello(user);
--- greeting.ts
+++ greeting.ts
@@ -1,8 +1,38 @@
-function sayHello(person: string): string {
-    return "Hello, " + person;
+
+
+
+// interface EmployeeOptions/Person 'only' name values required others are optional. 
+interface EmployeeOptions {
+    FirstName: string;
+    Lastname: string;
+    PhoneNumber?: string;
+    Location?: string;
+    State?: string;
+    ZipCode?: number;
+    Occupation?: string;
+    HourlyWages?: number;
+
 }
-var user = "Super Student";
-document.getElementById("para").innerHTML = sayHello(user);
+
+let createOptions = (
+    employeeOptions: EmployeeOptions
+): { FirstName: string; Lastname: string; PhoneNumber: string; Location: string; State: string; ZipCode: number; Occupation: string; HourlyWages: number;} => {
+    let newOptions = {
+        FirstName: 'fistName5', Lastname: 'lastName5',
+        PhoneNumber: 'none ', Location: 'none', State: 'none',
+        ZipCode: 0, Occupation: 'none', HourlyWages: 33
+    };
+
+    if (employeeOptions) {
+        newOptions.PhoneNumber = employeeOptions.PhoneNumber;
+        newOptions.Location = employeeOptions.Location;
+        newOptions.State = employeeOptions.State;
+        newOptions.ZipCode = employeeOptions.ZipCode;
+        newOptions.Occupation = employeeOptions.Occupation;
+        newOptions.HourlyWages = employeeOptions.HourlyWages
+    }
+    return newOptions;
+}
 
 
 // employee class
@@ -14,10 +44,11 @@ class Employee implements EmployeeOptions{
     State: string;
     ZipCode: number;
     Occupation: string;
-    hourlyWages: number = 33;
+    // add hourlyWage property to person/Employee class
+    HourlyWages: number = 33;
 
-    // 3. Create a constructor to initialize the props and 
-    constructor(firstName: string, lastName: string, phoneNumber: string, location: string, state: string, zipCode: number, occupation: string) {
+    // constructor to initialize the props and 
+    constructor(firstName: string, lastName: string, phoneNumber?: string, location?: string, state?: string, zipCode?: number, occupation?: string, hourlyWages?: number) {
         this.FirstName = firstName;
         this.Lastname = lastName;
         this.PhoneNumber = phoneNumber;
@@ -25,10 +56,11 @@ class Employee implements EmployeeOptions{
         this.State = state;
         this.ZipCode = zipCode;
         this.Occupation = occupation
-
+        this.HourlyWages = hourlyWages;
+    
     }
 
-    // methods to return combinations lik Full name, Name and Phone Number, Location, etc.
+    // methods to return combinations like Full name, Name and Phone Number, Location, etc.
     fullName(): string {
         return "Hi, this is " + this.FirstName + " " + this.Lastname;
     }
@@ -48,26 +80,49 @@ class Employee implements EmployeeOptions{
             " works as a " + this.Occupation + " in " + this.Location + ", " + this.State + " " + this.ZipCode;
     }
 
-
-  totalWages(hours: number): string {
-       
-        return ("Name: " + this.FirstName + " " + this.Lastname + "\n Weekly Wage: $" + this.hourlyWages * hours);
+    
+    // weekly wage with num hours optional param. 
+  totalWages(hours?: number): string { 
+        return ("Name: " + this.FirstName + " " + this.Lastname + "\n Weekly Wage: $" + this.HourlyWages * hours);
     };
 
 }
-// 5. Create several people and, output their information to <div> sections in html.
-var employee1 = new Employee("Jimmy ", " Hendrix ", "704-771-7387", "Atlanta", "Georgia", 17501, "Developer");
+// Create several people and, output their information to <div> sections in html.
+let employee1 = new Employee("Jimmy ", " Hendrix ", "704-771-7387", "Atlanta", "Georgia", 17501, "Developer");
 document.getElementById("e1").innerHTML = employee1.fullInfo();
-var employee2 = new Employee("Jonathan ", " Long ", "704-724-3817", "Charlotte", "North Carolina", 28212, "Analyst");
+let employee2 = new Employee("Jonathan ", " Long ", "704-724-3817", "Charlotte", "North Carolina", 28212, "Analyst", 12);
 document.getElementById("e2").innerHTML = employee2.fullInfo();
-var employee3 = new Employee("Noah ", "Jackson ", "516-523-7843", "Brooklyn", "New York", 11710, "Designer");
-document.getElementById("e3").innerHTML = employee3.fullInfo();
-var wages = new Employee("Jonathan ", " Long ", "704-724-3817", "Charlotte", "North Carolina", 28212, "Analyst");
-document.getElementById("e6").innerHTML = wages.totalWages(this.hours);
+let employee3 = new Employee("Noah ", "Jackson ");
+document.getElementById("e3").innerHTML = employee3.totalWages();
 
-// 6. Add hourlyWage property to person (and if you want, rename the class to Employee ). 
+// let employee13 = new Employee
 
-// 7. Add a function that calculates a weekly wage and takes the number of hours as an optional parameter. 
-// If not presented, use a typical 40hrs to calculate wage.Display this in a area that states name of person and weekly wage.
+//  wages here
+// let wages = new Employee("Jonathan ", " Long ", "704-724-3817", "Charlotte", "North Carolina", 28212, "Analyst");
 
 
+
+// let employee = new Employee("Noah ", "Jackson ");
+// document.getElementById("e4").innerHTML = employee3.fullInfo();
+
+// wages = document.getElementById("e6");
+// If not presented, use a typical 40hrs to calculate wage. Display this in a area that states name of person and weekly wage.
+
+
+// const employee7 = createOptions({
+//     FirstName: 'fistName7', Lastname: 'lastName7', PhoneNumber: 'num7 ', Location: 'Location7', State: 'State7',
+//     ZipCode: 11710, Occupation: 'Occupation7'
+// });
+
+// let employee8 = new Employee(FirstName: 'firstName8', Lastname: 'lastName8' );
+// document.getElementById("e6").textContent = employee8.Occupation;
+
+// console.log(employee7);
+// console.log(employee8)
+
+
+function sayHello(person: string): string {
+    return "Hello, " + person;
+}
+var user = "Super Student";
+document.getElementById("para").innerHTML = sayHello(user);
